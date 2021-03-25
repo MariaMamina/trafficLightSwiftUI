ae56572084d2b4e147189b00c45d045dc28581a8
Maria Mamina <maria.mamina3@gmail.com>
2021-03-25 09:09:18 +0200

    Initial Commit

---
+++ trafficLightSwiftUI/trafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightSwiftUIApp.swift
+//  trafficLightSwiftUI
+//
+//  Created by Maria Mamina on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
